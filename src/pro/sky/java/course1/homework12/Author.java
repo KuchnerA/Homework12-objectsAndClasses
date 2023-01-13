7505d9d4ef20a2b88b562f6a3b71d71114fe8571
Alexey <ale-kuchner@mail.ru>
2023-01-14 00:26:29 +0500

    Cделано ДЗ 12

---
+++ src/pro/sky/java/course1/homework12/Author.java
@@ -0,0 +1,20 @@
+package pro.sky.java.course1.homework12;
+
+public class Author {
+    private String authorFirstName;
+    private String authorLastName;
+
+    public Author(String authorFirstName, String authorLastName) {
+        this.authorFirstName = authorFirstName;
+        this.authorLastName = authorLastName;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+}
+
---
+++ src/pro/sky/java/course1/homework12/Book.java
@@ -0,0 +1,29 @@
+package pro.sky.java.course1.homework12;
+
+public class Book {
+    private String bookName;
+    private Author author;
+    private int publishingYear;
+
+    public Book(String bookName, Author author, int publishingYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthor() {
+        return author.getAuthorFirstName() + " " + author.getAuthorLastName();
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+}
